3b92f36579f56d094a8b3cc68f19bd5ff3541f38
67kid-glitch <jcwright014@gmail.com>
2025-12-27 09:48:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,2 @@
-const renderer = new Renderer();
-const world = new World(renderer.scene);
-const player = new Player(renderer.camera);
-
-const overlay = document.getElementById("click");
-
-overlay.addEventListener("click", () => {
-  overlay.style.display = "none";
-  renderer.renderer.domElement.requestPointerLock();
-});
-
-function loop() {
-  requestAnimationFrame(loop);
-  player.update();
-  renderer.render();
-}
-
-loop();
+document.body.style.background = "red";
+console.log("MAIN JS RUNNING");
