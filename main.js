3c278833443db1159f5f58b577509427de48dd91
67kid-glitch <jcwright014@gmail.com>
2025-12-27 10:28:41 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 const renderer = new Renderer();
-const world = new World(renderer.scene);
+
+let currentDimension;
+
+function loadDimension(name) {
+  if (currentDimension) currentDimension.destroy();
+
+  if (name === "nether") {
+    currentDimension = new Dimension("nether", renderer.scene, {
+      sky: 0x330000,
+      block: 0xaa0000
+    });
+  } else if (name === "end") {
+    currentDimension = new Dimension("end", renderer.scene, {
+      sky: 0x000000,
+      block: 0x555555
+    });
+  } else {
+    currentDimension = new Dimension("overworld", renderer.scene, {
+      sky: 0x87ceeb,
+      block: 0x55aa55
+    });
+  }
+}
+
+loadDimension("overworld");
+
 const player = new Player(renderer.camera);
 player.addToScene(renderer.scene);
 
-const mobs = new MobSystem(renderer.scene);
+// === TEMP DIMENSION SWITCH KEYS ===
+window.addEventListener("keydown", e => {
+  if (e.key === "1") loadDimension("overworld");
+  if (e.key === "2") loadDimension("nether");
+  if (e.key === "3") loadDimension("end");
+});
 
 const overlay = document.getElementById("click");
 overlay.addEventListener("click", () => {
@@ -14,7 +44,6 @@ overlay.addEventListener("click", () => {
 function loop() {
   requestAnimationFrame(loop);
   player.update();
-  mobs.update(player);
   renderer.render();
 }
 
