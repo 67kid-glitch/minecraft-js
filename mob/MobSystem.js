4c72eafa4f68ec12f9c80e02aff7889e7e4d12d4
67kid-glitch <jcwright014@gmail.com>
2025-12-27 10:36:30 -0800

    Update MobSystem.js

--- mob/MobSystem.js
+++ mob/MobSystem.js
@@ -1,16 +1,30 @@
 class MobSystem {
   constructor(scene) {
-    this.mobs = [];
     this.scene = scene;
+    this.mobs = [];
+    this.currentDimension = "overworld";
+  }
+
+  setDimension(name) {
+    // Remove existing mobs
+    this.mobs.forEach(m => m.destroy());
+    this.mobs = [];
+    this.currentDimension = name;
 
     MOB_DATA.forEach(data => {
-      this.mobs.push(new Mob(data, scene));
+      const allowed =
+        data.dimensions === "all" ||
+        data.dimensions.includes(name);
+
+      if (allowed) {
+        this.mobs.push(new Mob(data, this.scene));
+      }
     });
   }
 
-  update(player) {
+  update(player, camera) {
     this.mobs.forEach(mob => {
-      mob.update(player.body.position);
+      mob.update(player.body.position, camera);
     });
   }
 }
